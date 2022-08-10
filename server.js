43680ad5c8f662eb393ae71ec33476a911685cff
kathnava <kathnavafr@gmail.com>
2022-08-10 14:39:50 +0200

    10-08-first

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser  = require('body-parser');
+const path = require('path');
+const apiRouter = require('./server/route/apiRouter').router;
+const cors = require('cors');
+const userfetch = require('./userfetch');
+
+const corsOptions = {
+    'Access-Control-Allow-Origin': '*'
+}
+
+// Instantiate server
+var server = express();
+
+// Body Parser configuration
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cookieParser());
+server.use(cors(corsOptions))
+
+
+// config view engine
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+server.set('/img', path.join(__dirname + 'public/img'));
+
+
+server.use(express.static(path.join(__dirname + '/public')));
+
+// Declare view routes
+server.get('/register', (req, res) => {
+    res.render('register')
+    
+});
+server.get('/login', (req, res) => {
+    res.render('connexion')
+});
+server.get('/', (req, res) => {
+    res.render('home')
+});
+
+// Declare API routes
+server.use('/', apiRouter);
+
+server.listen(3500, function() {
+    console.log('Server en écoute :)');
+})
